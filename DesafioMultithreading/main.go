ac9278687be775d2bc43d1fd709b2351aadba6bf
Raul Soares de Carvalho <raulcarvalho58@gmail.com>
2024-11-26 12:32:39 -0300

    Update main.go

--- DesafioMultithreading/main.go
+++ DesafioMultithreading/main.go
@@ -8,31 +8,6 @@ import (
 	"time"
 )
 
-/*type ViaCEP struct {
-	Cep         string `json:"cep"`
-	Logradouro  string `json:"logradouro"`
-	Complemento string `json:"complemento"`
-	Unidade     string `json:"unidade"`
-	Bairro      string `json:"bairro"`
-	Localidade  string `json:"localidade"`
-	Uf          string `json:"uf"`
-	Estado      string `json:"estado"`
-	Regiao      string `json:"regiao"`
-	Ibge        string `json:"ibge"`
-	Gia         string `json:"gia"`
-	Ddd         string `json:"ddd"`
-	Siafi       string `json:"siafi"`
-}*/
-
-/*type BrasilAPI struct {
-	Cep          string `json:"cep"`
-	State        string `json:"state"`
-	City         string `json:"city"`
-	Neighborhood string `json:"neighborhood"`
-	Street       string `json:"street"`
-	Service      string `json:"service"`
-}*/
-
 func main() {
 	c1 := make(chan *http.Response)
 	c2 := make(chan *http.Response)
